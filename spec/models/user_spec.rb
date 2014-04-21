d0e9c9732d937e5e028fda2b3ce40f7c8994ff75
Jacob Lopez <jlopez@chicagotechacademy.org>
2014-04-21 14:56:00 -0500

    part 14 finished

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,16 @@ require 'spec_helper'
 
 describe User do
  
-  before { @user = User.new(name: "Example User", email: "user@example.com")}
+  before { @user = User.new(name: "Example User", email: "user@example.com", 
+  password: "fubar", password_confirmation: "fubar") }
   
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email)}
+  it { should respond_to(:password_digest)}
+  it { should respond_to(:password)}
+  it { should respond_to(:password_confirmation)}
 
   it { should be_valid }
 
@@ -28,9 +32,51 @@ describe User do
 
   describe "when email format is invalid" do
     it "should be invalid" do 
-        addresses = %w[user@foo,com user_at_foo.org exapmle.use@foo.com]
-        addresses.each = invalid_address
-        
+        addresses = %w[user@foo,com user_at_foo.org exapmle.use@foo.]
+        addresses.each do |invalid_address|
+            @user.email = invalid_address
+            @user.should_not be_valid
+        end
     end
   end
+
+  describe "when email format is valid" do
+    it "should be valid" do 
+      addresses = %w[user@foo.com user@foo.jp example+me@foo.edu]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        @user.should be_valid
+    end
+    end
+  end
+
+  describe "email address is taken" do
+      before do 
+        user_with_same_email = @user.dup 
+        user_with_same_email.email = @user.email.upcase
+        user_with_same_email.save 
+      end
+
+     it { should_not be_valid }
+  end
+
+   describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " "}
+    it { should_not be_valid }
+   end
+
+   describe "when passwords mismatch" do 
+     before { @user.password_confirmation = "mismatch"}
+     it{ should_not be_valid }
+   end
+  
+    describe "when confirmation is nil" do
+     before {@user.password_confirmation = nil}
+     it { should_not be_valid }
+   end
+
+   describe "when password is too small" do
+    before {@user.password = @user.password_confirmation = "a" * 4}
+    it { should_not be_valid }
+   end
 end
